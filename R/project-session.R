6fc3e74b1fde20b5efa1412c137da563698ad720
Ajenthen,G,Ranjan <ajenthen@gmail.com>
2021-06-15 15:15:21 +0200

    Start of project

---
+++ R/project-session.R
@@ -0,0 +1,16 @@
+source(here::here("R/package-loading.r")
+
+# basics of R --------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos
+c("a", "b", "c")
+c("true", "false")
+c(1, 2, 4)
+factor(c("high", "medium", "low"))
+head (CO2, 10)
+colnames(CO2)
+str(CO2)
+summary(CO2)
+
+source(here::"R")
