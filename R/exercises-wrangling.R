36f65a2a467945a8d1473c5856ba5a2171be25cc
Ajenthen,G,Ranjan <ajenthen@gmail.com>
2021-06-16 13:42:33 +0200

    TASK 9.16

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -42,5 +42,9 @@ nhanes_small %>%
     rename(diabetes_diagnosis_age = diabetes_age)
 
 nhanes_small %>%
-    select(bmi) %>%
-    contains("age")
+    summarise(mean_weight = mean(weight, na.rm=TRUE),
+              mean_age = mean(age, na.rm=TRUE),
+              max_height = max(height, na.rm=TRUE),
+              min_height = min(height, na.rm=TRUE),
+              Med_pa = median(phys_active_days, na.rm=TRUE),
+              median_age= median(age, na.rm=TRUE))
