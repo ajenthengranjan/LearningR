e06b01038f662e740d9ea5b18f326e4d0f70ab81
Ajenthen,G,Ranjan <ajenthen@gmail.com>
2021-06-16 10:47:13 +0200

    Renamin columns to snakecase and renamin specific columns

--- R/wranglig-session.R
+++ R/wranglig-session.R
@@ -19,3 +19,30 @@ select(NHANES, ends_with("Day"))
 
 select(NHANES, contains("Age"))
 ?select_helpers
+
+#Make a smaller dataset
+# Save the selected columns as a new data frame
+# Recall the style guide for naming objects
+nhanes_small <- select(NHANES, Age, Gender, Height,
+                       Weight, BMI, Diabetes, DiabetesAge,
+                       PhysActiveDays, PhysActive, TotChol,
+                       BPSysAve, BPDiaAve, SmokeNow, Poverty)
+
+# View the new data frame
+nhanes_small
+
+
+
+#Renaming columns
+
+#to snakecase
+nhanes_small <- rename_with(nhanes_small,
+                            snakecase::to_snake_case)
+colnames(nhanes_small)
+colnames(NHANES)
+
+#Renaming secific columns
+
+nhanes_small <- rename(nhanes_small, sex=gender)
+
+colnames(nhanes_small)
