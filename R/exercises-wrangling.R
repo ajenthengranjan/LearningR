ce8ef359a7728699ebbf951538825dd039d67cf3
Ajenthen,G,Ranjan <ajenthen@gmail.com>
2021-06-16 09:50:07 +0200

    COLnames

---
+++ R/exercises-wrangling.R
@@ -0,0 +1,15 @@
+# Load the packages
+source(here::here("R/package-loading.R"))
+
+# Check column names
+colnames(NHANES)
+
+# Look at contents
+str(NHANES)
+glimpse(___)
+
+# See summary
+summary(___)
+
+# Look over the dataset documentation
+?___
